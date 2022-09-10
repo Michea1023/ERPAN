d16f275455111113ec0cce340250c42880a91ee6
seba6453 <sebastian.astudillo@alumnos.ucn.cl>
2022-09-10 00:48:04 -0300

    install backend

---
+++ backend/src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+const app = express();
+
+app.use(express.json()); //middleware que trasforma la req.body a un jason
+
+
+const PORT = 3001;
+
+app.get('/ping', (_req, res) => {
+    console.log("ya esta listo!!! ");
+    res.send('pong');
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Express server listening on port ${PORT}`);
+});
