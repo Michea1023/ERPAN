ad029b4d102eb219a24efffebeb8fa7e9ac664b5
seba6453 <sebastian.astudillo@alumnos.ucn.cl>
2022-09-22 17:15:11 -0300

    Update dataBase.ts

--- backend/src/dataBase.ts
+++ backend/src/dataBase.ts
@@ -1,13 +1,22 @@
 const { Pool } = require('pg');
 
 
-
+/*
 const connectionData = {
     user: 'postgres',
     host: 'localhost',
     database: 'DB_erpan',
     password: 'admin',
     port: 5432,
+};
+*/
+
+const connectionData = {
+    user: 'postgres',
+    host: 'containers-us-west-70.railway.app',
+    database: 'railway',
+    password: 'trf1RRKWxrdGyMeNg6Yr',
+    port: 6604,
 };
 
 
