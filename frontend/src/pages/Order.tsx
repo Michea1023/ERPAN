1ce3a350cbe17f3a0f19acb9bc1a785bf57cf857
SouLKy <pipapu9900@gmail.com>
2022-11-05 18:34:38 -0300

    Order Card

--- frontend/src/pages/Order.tsx
+++ frontend/src/pages/Order.tsx
@@ -1,8 +1,41 @@
+import { Button } from 'react-bootstrap'
+
 /*
 renders the order page
 @params {Props} ...
 @returns {JSX.Element}
 */
 export default function Order() {
-  return <div className='prueba'>hola</div>
+  return (
+    <>
+      <div>
+        <div className='container mt-2'>
+          <div className='container'>
+            <nav className='navbar navbar-light bg-light'>
+              <form className='d-flex flex-row'>
+                <input
+                  className='form-control mr-sm-2'
+                  type='search'
+                  placeholder='Search'
+                  aria-label='Search'
+                ></input>
+                <button
+                  className='btn btn-outline-success my-2 my-sm-0'
+                  type='submit'
+                >
+                  Search
+                </button>
+              </form>
+            </nav>
+            <ul>
+              <li>
+                <div className='name'>uuuwu</div>
+                <div className='price'>aaaaaaaaa</div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
