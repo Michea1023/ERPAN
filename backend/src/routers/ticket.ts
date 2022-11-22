e215f560990cb856e696c8e43bc3df34478dac0d
seba6453 <sebastian.astudillo@alumnos.ucn.cl>
2022-11-22 09:58:59 -0300

    update ticket

--- backend/src/routers/ticket.ts
+++ backend/src/routers/ticket.ts
@@ -3,7 +3,7 @@ import { decodeToken } from "../middleware/token";
 import verifyToken from "../middleware/verifyToken";
 //import { verifyStock } from "../services/productServices";
 import { addTicketDetail } from "../services/ticketDetailServices";
-import { addTicket, deleteTicket, getAll } from "../services/ticketServices";
+import { addTicket, deleteTicket, getAll, getTicket } from "../services/ticketServices";
 import { NewTicketDetail } from "../types/ticketDetail_types";
 import { NewTicket } from "../types/ticket_types";
 const dotenv = require('dotenv');
@@ -21,6 +21,16 @@ router.get("/",async (req,res) => {
     const dataToken = decodeToken(req.get("Authorization")?.substring(7));
     const tickets = await getAll(dataToken.id);
     res.status(200).send(tickets);
+})
+
+router.get("/:id",async (req,res) => {
+    const dataToken = decodeToken(req.get("Authorization")?.substring(7));
+    const ticket = await getTicket(parseInt(req.params.id), dataToken.id);
+    if (ticket != undefined) {
+        res.status(200).send(ticket);
+    } else {
+        res.status(404).send({"mensaje":"Ticket no encontrado"});
+    }
 })
 
 /* This is a route that is used to add a new ticket. */
