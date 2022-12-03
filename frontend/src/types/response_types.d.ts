4f7f82023b8c7987fe24f80246e2e2b11d0374a1
Michea1023 <64218523+Michea1023@users.noreply.github.com>
2022-12-03 18:11:33 -0400

    rename product name in ticket detail interface

--- frontend/src/types/response_types.d.ts
+++ frontend/src/types/response_types.d.ts
@@ -3,47 +3,47 @@ this file contains the response types
  */
 
 export interface Business_Response {
-  name_business: string
-  token: string
+    name_business: string
+    token: string
 }
 
 export type ProductList = Array<ProductResponse>
 
 export interface ProductResponse {
-  id: number
-  id_categories: string
-  id_providers: string
-  bar_code: string
-  stock: number
-  name_product: string
-  price: number
-  cost: number
+    id: number
+    id_categories: string
+    id_providers: string
+    bar_code: string
+    stock: number
+    name_product: string
+    price: number
+    cost: number
 }
 
 export interface ProviderResponse {
-  name_providers: string
-  id_business: number
+    name_providers: string
+    id_business: number
 }
 
 export interface CategoryResponse {
-  name_categories: string
+    name_categories: string
 }
 
 export type TicketList = Array<Ticket>
 
 export interface Ticket {
-  id: Number
-  id_business: Number
-  general_price: number
-  selled_date: String
-  selled_time: String
+    id: Number
+    id_business: Number
+    general_price: number
+    selled_date: String
+    selled_time: String
 }
 
 export interface TicketDetail {
-  id: number
-  id_product: number
-  id_ticket: number
-    product_name: string
-  total_price: string
-  amount: number
+    id: number
+    id_product: number
+    id_ticket: number
+    name_product: string
+    total_price: string
+    amount: number
 }
